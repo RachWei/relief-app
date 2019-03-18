38730cabc08d890433fc91c7076e0cad8609b9e3
Rach looka Wei <rachael@weis-mbp13.local>
2019-03-17 20:39:39 -0400

    Initial Commit

---
+++ relief/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  relief
+//
+//  Created by Rachael Wei on 3/17/19.
+//  Copyright © 2019 technovation thursdays. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
